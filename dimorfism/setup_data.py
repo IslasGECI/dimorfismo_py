7bc3bc66c3ddf3af8384a82e6fc81491f425bc62
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-09-24 19:49:03 +0000

    🔥 delete template tests

--- dimorfism/setup_data.py
+++ dimorfism/setup_data.py
@@ -1,9 +1,5 @@
 import pandas as pd
 
 
-def add_offset(augend: int, addend: int) -> int:
-    return augend + addend
-
-
 def split_data(df: pd.DataFrame) -> dict:
     return {}
--- tests/test_setup_data.py
+++ tests/test_setup_data.py
@@ -2,14 +2,6 @@ import pandas as pd
 import dimorfism as dt
 
 
-def test_add_offset():
-    augend = 1
-    addend = 2
-    expected = augend + addend
-    obtained = dt.add_offset(augend, addend)
-    assert expected == obtained
-
-
 def test_split_data() -> None:
     full_data: pd.DataFrame = pd.read_csv(
         "/workdir/tests/data/laysan_albatross_morphometry_guadalupe.csv"
