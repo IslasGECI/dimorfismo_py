2001becd03b6556d5f8deef42f5577dd70409457
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-09-24 19:55:36 +0000

    🛑🧪 Fail tests

--- tests/test_setup_data.py
+++ tests/test_setup_data.py
@@ -6,4 +6,5 @@ def test_split_data() -> None:
     full_data: pd.DataFrame = pd.read_csv(
         "/workdir/tests/data/laysan_albatross_morphometry_guadalupe.csv"
     )
-    _: dict = dt.split_data(full_data)
+    splited_data: dict = dt.split_data(full_data)
+    splited_data["to_test"]
