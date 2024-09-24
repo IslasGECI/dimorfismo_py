8e0a6b2c8bbd292ae688d81adf9a4f3849f16aa8
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-09-24 20:06:19 +0000

    ✅ Pass tests

--- dimorfism/setup_data.py
+++ dimorfism/setup_data.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 
 def split_data(df: pd.DataFrame) -> dict:
-    splitted_data = {"to_test": df[:3]}
+    splitted_data = {"to_fit": df[:12], "to_test": df[12:]}
 
     return splitted_data
