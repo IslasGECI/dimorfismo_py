00dea365d287ed40d349df699a96cfebe2f2c53a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-09-24 20:59:29 +0000

    🛑🧪 Fail tests

--- tests/test_setup_data.py
+++ tests/test_setup_data.py
@@ -44,7 +44,7 @@ def test_split_data_are_differents_rows() -> None:
     fit_data: pd.DataFrame = splited_data["to_fit"]
     index_to_fit = set(fit_data)
     index_to_test = set(test_data)
-    obtained_commun = len(index_to_fit & index_to_test)
+    obtained_commun = len(index_to_fit and index_to_test)
     expected_commun = 0
     assert obtained_commun == expected_commun
 
