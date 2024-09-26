89ef60e698eeec30e8c4f0a208b198eda056020e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-09-26 17:11:14 +0000

    🛑🧪 Fail tests

--- tests/test_fit_logistic_regression.py
+++ tests/test_fit_logistic_regression.py
@@ -3,8 +3,9 @@ import dimorfism as dt
 
 
 def test_fit_logistic_regression():
-    X = numpy.array(
+    x = numpy.array(
         [3.78, 2.44, 2.09, 0.14, 1.72, 1.65, 4.92, 4.37, 4.96, 4.52, 3.69, 5.88]
     ).reshape(-1, 1)
     y = numpy.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
-    _ = dt.logistic_regression()
+    logr = dt.logistic_regression()
+    logr.fit(x, y)
