f9ce91a15b2d2791e8a644468d857692b3857456
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-09-26 17:02:53 +0000

    🛑🧪 Fail tests

---
+++ tests/test_fit_logistic_regression.py
@@ -0,0 +1,5 @@
+def test_fit_logistic_regression():
+    X = numpy.array(
+        [3.78, 2.44, 2.09, 0.14, 1.72, 1.65, 4.92, 4.37, 4.96, 4.52, 3.69, 5.88]
+    ).reshape(-1, 1)
+    y = numpy.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
