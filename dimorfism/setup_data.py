c898b9fba116d3a6f63acc725062d24efb3b5502
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-09-26 19:21:54 +0000

    ✅ Pass tests

--- dimorfism/setup_data.py
+++ dimorfism/setup_data.py
@@ -2,7 +2,11 @@ import pandas as pd
 
 
 def split_data(df: pd.DataFrame) -> dict:
-    splitted_data = {"to_fit": obtained_to_fit_data(df), "to_test": obtained_to_test_data(df)}
+    splitted_data = {
+        "to_fit": obtained_to_fit_data(df),
+        "to_test": obtained_to_test_data(df),
+        "to_test_y": obtained_to_test_data_y(df),
+    }
 
     return splitted_data
 
@@ -17,6 +21,15 @@ def obtained_to_fit_data(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def obtained_to_test_data(df: pd.DataFrame) -> pd.DataFrame:
+
+    to_test = df.drop(df.sample(frac=0.8, random_state=7).index)
+
+    not_wanted_colnames: list = ["id_nido", "id_darvic", "subcolonia", "temporada", "notas", "sexo"]
+
+    return to_test.drop(columns=not_wanted_colnames)
+
+
+def obtained_to_test_data_y(df: pd.DataFrame) -> pd.DataFrame:
 
     to_test = df.drop(df.sample(frac=0.8, random_state=7).index)
 
