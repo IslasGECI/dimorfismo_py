824839d6c6c5bc86fd889f986111e78c8b92aeb5
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-09-26 18:34:24 +0000

    ✅ Pass tests

--- tests/test_fit_logistic_regression.py
+++ tests/test_fit_logistic_regression.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import numpy
 import pytest
 import dimorfism as dt
@@ -17,3 +18,20 @@ def test_logistic_regression():
     expected_odd = numpy.array([[4.035]])
     obtained_odd = numpy.exp(log_odds)
     assert expected_odd == pytest.approx(obtained_odd, 0.1), "Second example of w3school"
+
+
+full_data: pd.DataFrame = pd.read_csv(
+    "/workdir/tests/data/laysan_albatross_morphometry_guadalupe.csv"
+)
+splited_data: dict = dt.split_data(full_data)
+
+
+def test_albatross_example():
+    full_data = splited_data["to_fit"]
+    y = full_data["sexo"]
+    x = full_data.drop(columns="sexo")
+    logr = dt.logistic_regression()
+    logr.fit(x, y)
+    expected = ["M"]
+    obtained = logr.predict(x[:1])
+    assert obtained == expected, "First real example"
