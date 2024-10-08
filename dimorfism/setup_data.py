55a39fcd5a470ab94d88fc62fad26dbe5227a369
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-08 19:50:27 +0000

    ♻️  Refactor

--- dimorfism/setup_data.py
+++ dimorfism/setup_data.py
@@ -4,11 +4,22 @@ from sklearn.model_selection import train_test_split
 
 def split_data(df: pd.DataFrame) -> dict:
     target = df["sexo"]
-    data_train, data_test, target_train, target_test = train_test_split(df, target, random_state=7)
+    data_train, data_test, target_train, target_test = train_test_split(
+        df, target, random_state=7, train_size=0.8
+    )
+    not_wanted_colnames: list = ["id_nido", "id_darvic", "subcolonia", "temporada", "notas"]
+    not_wanted_colnames_tests: list = [
+        "id_nido",
+        "id_darvic",
+        "subcolonia",
+        "temporada",
+        "notas",
+        "sexo",
+    ]
     splitted_data = {
-        "to_fit": obtained_to_fit_data(df),
-        "to_test": obtained_to_test_data(df),
-        "to_test_y": obtained_to_test_data_y(df),
+        "to_fit": data_train.drop(columns=not_wanted_colnames),
+        "to_test": data_test.drop(columns=not_wanted_colnames_tests),
+        "to_test_y": pd.DataFrame(target_test),
     }
 
     return splitted_data
