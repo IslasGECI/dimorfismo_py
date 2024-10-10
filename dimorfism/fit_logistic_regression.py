839cb2f7c78b5acd1f0cd737987bfd65b23951c6
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-10 18:49:19 +0000

    ✅ Pass tests

--- dimorfism/fit_logistic_regression.py
+++ dimorfism/fit_logistic_regression.py
@@ -1,4 +1,12 @@
 from sklearn import linear_model
+import pandas as pd
+
+
+def predicted_sex(splited_data: pd.DataFrame):
+    model = logistic_regression()
+    x = splited_data["to_fit"].drop(columns="sexo")
+    y = splited_data["to_fit"]["sexo"]
+    return model.fit(x, y)
 
 
 def logistic_regression():
