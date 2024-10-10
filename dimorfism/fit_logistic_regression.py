7e283ec085ea3b571c53b5811fed64dc517e5ed3
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-10 19:26:06 +0000

    ♻️  Refactor

--- dimorfism/fit_logistic_regression.py
+++ dimorfism/fit_logistic_regression.py
@@ -3,13 +3,18 @@ import pandas as pd
 
 
 def predicted_sex(splited_data: pd.DataFrame):
+    fitted_model = get_fitted_model(splited_data)
+
+    to_predict = splited_data["to_test"]
+    return fitted_model.predict(to_predict)
+
+
+def get_fitted_model(splited_data):
     model = logistic_regression()
     x = splited_data["to_fit"]
     y = splited_data["to_fit_target"]
     fitted_model = model.fit(x, y)
-
-    to_predict = splited_data["to_test"]
-    return fitted_model.predict(to_predict)
+    return fitted_model
 
 
 def logistic_regression():
