4ce2ed5c90904b63529f846c04f7bc205fdfa233
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-26 22:08:04 +0000

    ♻️  Refactor

--- dimorfism/setup_data.py
+++ dimorfism/setup_data.py
@@ -8,14 +8,6 @@ def split_data(df: pd.DataFrame) -> dict:
     data_train, data_test, target_train, target_test = train_test_split(
         df, target, random_state=7, train_size=0.8
     )
-    not_wanted_colnames: list = [
-        "id_nido",
-        "id_darvic",
-        "subcolonia",
-        "temporada",
-        "notas",
-        target_name,
-    ]
     wanted_colnames: list = [
         "bill_depth",
         "bill_length",
