78898e8bf4b6e2fd5b70012fb8b960c46d8a16e9
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-28 19:03:49 +0000

    🛑🧪 Fail tests

--- tests/test_write_parameters.py
+++ tests/test_write_parameters.py
@@ -8,12 +8,18 @@ def test_obtained_parameters():
     fitted_model = Mock_fitted_model()
     obtained = dt.obtained_parameters(fitted_model)
     isinstance(obtained, dict)
-    assert list(obtained.keys()) == ["bill_depth", "bill_length", "Tarsus", "head_width"]
+    assert list(obtained.keys()) == [
+        "bill_depth",
+        "bill_length",
+        "Tarsus",
+        "head_width",
+        "Intercept",
+    ]
 
     assert list(obtained.values()) == coeficients
 
 
 class Mock_fitted_model:
     def __init__(self):
-        self.intercept_ = [1.26]
+        self.intercept_ = [-12.466]
         self.coef_ = coeficients
