799f03cdedb61cf99b5963fbcd201a0e7d260546
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-28 18:38:42 +0000

    🛑🧪 Fail tests

--- tests/test_write_parameters.py
+++ tests/test_write_parameters.py
@@ -2,4 +2,12 @@ import dimorfism as dt
 
 
 def test_obtained_parameters():
-    _ = dt.obtained_parameters()
+    fitted_model = Mock_fitted_model()
+    obtained = dt.obtained_parameters(fitted_model)
+    isinstance(obtained, dict)
+
+
+class Mock_fitted_model:
+    def __init__(self):
+        self.intercept_ = [1.26]
+        self.coef_ = [1.26, 3.45]
