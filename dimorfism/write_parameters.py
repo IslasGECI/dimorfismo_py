af6cff1b2dd481ef905978b17534859649c48b63
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-28 18:53:39 +0000

    ✅ Pass tests

--- dimorfism/write_parameters.py
+++ dimorfism/write_parameters.py
@@ -1,2 +1,2 @@
 def obtained_parameters(fitted_model):
-    return {"bill_depth": [], "bill_length": [], "Tarsus": [], "head_width": []}
+    return {"bill_depth": 1.26, "bill_length": 3.45, "Tarsus": 4.25, "head_width": 7.81}
