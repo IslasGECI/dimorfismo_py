92ff7b9da751aba7821e0534a9e33c66937f1d1c
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-28 19:11:42 +0000

    ♻️  Refactor

--- dimorfism/write_parameters.py
+++ dimorfism/write_parameters.py
@@ -1,6 +1,4 @@
 def obtained_parameters(fitted_model):
-    keys = ["bill_depth", "bill_length", "Tarsus", "head_width"]
-    values = fitted_model.coef_
-    myDict = {k: v for (k, v) in zip(keys, values)}
-    myDict["Intercept"] = fitted_model.intercept_[0]
-    return myDict
+    keys = ["bill_depth", "bill_length", "Tarsus", "head_width", "Intercept"]
+    values = [*fitted_model.coef_, *fitted_model.intercept_]
+    return {k: v for (k, v) in zip(keys, values)}
