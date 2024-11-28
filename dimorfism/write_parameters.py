b7495805be606ede1e2823276429a928d155560d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-28 19:10:04 +0000

    ✅ Pass tests

--- dimorfism/write_parameters.py
+++ dimorfism/write_parameters.py
@@ -2,5 +2,5 @@ def obtained_parameters(fitted_model):
     keys = ["bill_depth", "bill_length", "Tarsus", "head_width"]
     values = fitted_model.coef_
     myDict = {k: v for (k, v) in zip(keys, values)}
-
+    myDict["Intercept"] = fitted_model.intercept_[0]
     return myDict
