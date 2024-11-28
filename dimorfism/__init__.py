049e0714d83f092a70af6182fa65465fc2a5d526
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-28 19:43:15 +0000

    ✅ Pass tests

--- dimorfism/__init__.py
+++ dimorfism/__init__.py
@@ -3,4 +3,4 @@
 __version__ = "0.0.1"
 from .setup_data import *  # noqa
 from .fit_logistic_regression import *  # noqa
-from .write_parameters import obtained_parameters  # noqa
+from .write_parameters import obtained_parameters, write_json_parameters  # noqa
--- dimorfism/write_parameters.py
+++ dimorfism/write_parameters.py
@@ -2,3 +2,7 @@ def obtained_parameters(fitted_model):
     keys = ["bill_depth", "bill_length", "Tarsus", "head_width", "Intercept"]
     values = [*fitted_model.coef_, *fitted_model.intercept_]
     return {k: v for (k, v) in zip(keys, values)}
+
+
+def write_json_parameters(parameters_dictionary, parameters_path):
+    pass
