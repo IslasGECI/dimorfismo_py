f9c7f7c90303809fcadb2756224c231611013615
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-11 17:24:57 +0000

    ✅ Pass tests

--- dimorfism/cli.py
+++ dimorfism/cli.py
@@ -1,7 +1,12 @@
 from dimorfism.write_parameters import get_model_parameters, write_json_parameters
 import pandas as pd
 
+import typer
 
+app = typer.Typer()
+
+
+@app.command()
 def write_model_parameters(data_path, parameters_path):
     complete_dataframe = pd.read_csv(data_path)
     parameters_dictionary = get_model_parameters(complete_dataframe)
