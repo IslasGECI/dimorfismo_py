a1c03ddaee0019ccedf0009ba5094ce78099bb8f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-12 18:02:50 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 from dimorfism.cli import write_model_parameters, app
+import json
 
 import geci_test_tools as gtt
 from typer.testing import CliRunner
@@ -17,6 +18,7 @@ def tests_write_model_parameters():
 
     data_path = "tests/data/laysan_albatross_morphometry_guadalupe.csv"
     parameters_path = "tests/data/cli_model_parameters.json"
+
     gtt.if_exist_remove(parameters_path)
     result = runner.invoke(
         app,
@@ -24,6 +26,10 @@ def tests_write_model_parameters():
     )
     assert result.exit_code == 0
     gtt.assert_exist(parameters_path)
+
+    parameters = read_json(parameters_path)
+    assert parameters.keys() == ["bill_depth", "bill_length", "Tarsus", "head_width"]
+
     gtt.if_exist_remove(parameters_path)
 
 
@@ -34,3 +40,9 @@ def test_version():
     )
     expected_version = "0.0.1"
     assert expected_version in result.stdout
+
+
+def read_json(json_path):
+    with open(json_path, "r") as file:
+        data = json.load(file)
+    return data
