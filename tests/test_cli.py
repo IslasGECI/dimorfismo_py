c70fc43d916b1f025d56a8fce770b1532ba7f6af
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-12 16:33:32 +0000

    ♻️  Refactor:

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,14 +4,6 @@ import geci_test_tools as gtt
 from typer.testing import CliRunner
 
 
-def test_get_model_parameters():
-    data_path = "tests/data/laysan_albatross_morphometry_guadalupe.csv"
-    parameters_path = "tests/data/model_parameters.json"
-    gtt.if_exist_remove(parameters_path)
-    write_model_parameters(data_path, parameters_path)
-    gtt.assert_exist(parameters_path)
-
-
 runner = CliRunner()
 
 
