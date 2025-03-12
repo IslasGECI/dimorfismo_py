595835bb36977e8407a2cd434cd8375940fea2cc
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-12 18:53:19 +0000

    ♻️  Refactor:

--- dimorfism/fit_logistic_regression.py
+++ dimorfism/fit_logistic_regression.py
@@ -1,12 +1,4 @@
 from sklearn import linear_model
-import pandas as pd
-
-
-def predicted_sex(splited_data: pd.DataFrame):
-    fitted_model = get_fitted_model(splited_data)
-
-    to_predict = splited_data["to_test"]
-    return fitted_model.predict(to_predict)
 
 
 def get_fitted_model(splited_data):
