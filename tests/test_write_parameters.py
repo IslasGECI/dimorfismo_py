4deb7f8f740f4d6ae7da368b84b44a82ee874936
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-12 17:46:07 +0000

    ♻️  Refactor:

--- tests/test_write_parameters.py
+++ tests/test_write_parameters.py
@@ -3,14 +3,6 @@ import dimorfism as dt
 import geci_test_tools as gtt
 
 
-def test_write_json_parameters():
-    parameters_dictionary = {"Intercept": 1, "parameter_1": 8.4}
-    parameters_path = "tests/data/model_parameters.json"
-    gtt.if_exist_remove(parameters_path)
-    dt.write_json_parameters(parameters_dictionary, parameters_path)
-    gtt.assert_exist(parameters_path)
-
-
 coeficients = [1.26, 3.45, 4.25, 7.81]
 intercept = [-12.466]
 
