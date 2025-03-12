6a7df9a0acd284d93a93da8ea42ceead8813a45b
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-12 18:43:06 +0000

    ♻️  Refactor:

--- dimorfism/setup_data.py
+++ dimorfism/setup_data.py
@@ -3,23 +3,13 @@ from sklearn.model_selection import train_test_split
 
 
 def split_data(df: pd.DataFrame) -> dict:
-    target_name = "sexo"
-    target = df[target_name]
-    data_train, data_test, target_train, target_test = train_test_split(
-        df, target, random_state=7, train_size=0.8
-    )
     wanted_colnames: list = [
         "bill_depth",
         "bill_length",
         "head_width",
         "Tarsus",
     ]
-    splitted_data = {
-        "to_fit": data_train[wanted_colnames],
-        "to_fit_target": target_train,
-        "to_test": data_test[wanted_colnames],
-        "to_test_y": pd.DataFrame(target_test),
-    }
+    splitted_data = xxsplit_data(df, wanted_colnames)
 
     return splitted_data
 
