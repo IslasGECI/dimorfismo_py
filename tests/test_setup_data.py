7f34de67abe07ec0f06482ac4ac77c497e325978
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-18 18:01:26 +0000

    ♻️  Refactor:

--- tests/test_setup_data.py
+++ tests/test_setup_data.py
@@ -12,7 +12,8 @@ wanted_colnames: list = [
     "Tarsus",
 ]
 
-splited_data: dict = dt.split_data(full_data, wanted_colnames)
+filtered_data = full_data[wanted_colnames + ["sexo"]]
+splited_data: dict = dt.xxsplit_data(filtered_data)
 
 
 def test_split_data() -> None:
