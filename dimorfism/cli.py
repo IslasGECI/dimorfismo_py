717a373155406579dcaf6a539b7a2987dc8a370d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-19 16:36:46 +0000

    ♻️  Refactor: 🚚move adapter function

--- dimorfism/cli.py
+++ dimorfism/cli.py
@@ -1,5 +1,5 @@
 from dimorfism.write_parameters import get_model_parameters, write_json_parameters
-import pandas as pd
+from dimorfism.adapters import adapter_morphometry
 
 import typer
 
@@ -14,17 +14,6 @@ def write_model_parameters(
     selected_dataframe = adapter_morphometry(data_path)
     parameters_dictionary = get_model_parameters(selected_dataframe)
     write_json_parameters(parameters_dictionary, parameters_path)
-
-
-def adapter_morphometry(data_path):
-    wanted_colnames: list = [
-        "bill_depth",
-        "bill_length",
-        "head_width",
-        "Tarsus",
-    ]
-    complete_dataframe = pd.read_csv(data_path)
-    return complete_dataframe[wanted_colnames + ["sexo"]]
 
 
 @app.command()
