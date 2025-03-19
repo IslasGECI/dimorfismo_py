163c712f041a2b7c755e4e5197ab8075d7eaf257
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-19 16:31:38 +0000

    ♻️  Refactor:

--- dimorfism/write_parameters.py
+++ dimorfism/write_parameters.py
@@ -3,7 +3,7 @@ from dimorfism.setup_data import split_data
 from dimorfism.fit_logistic_regression import get_fitted_model
 
 
-def xxget_model_parameters(filtered_data):
+def get_model_parameters(filtered_data):
     splited_data = split_data(filtered_data)
     fitted_model = get_fitted_model(splited_data)
     parameters_dictionary = obtained_parameters(fitted_model)
