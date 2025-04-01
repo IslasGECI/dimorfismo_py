a55648c182150d586923e45d080a0dd93d99b80b
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-04-01 17:53:33 +0000

    🛑🧪 Fail tests

--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -5,4 +5,5 @@ def test_tdp_adapter():
     raw_data_path = "tests/data/tdp_morfometria_albatros.csv"
     obtained = raw_data_adapter(raw_data_path)
     obtained_columns = obtained.columns
-    assert "bill_length" in obtained_columns
+    expected_columns = ["bill_length", "bill_depth", "head_width", "Tarsus"]
+    assert all(expected_columns in obtained_columns)
