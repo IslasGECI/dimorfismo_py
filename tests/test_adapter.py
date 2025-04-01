35b0a812f0558a987891d8748b4e9905ae5c8f07
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-04-01 17:43:26 +0000

    🛑🧪 Fail tests

--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -4,3 +4,5 @@ from dimorfism.adapters import raw_data_adapter
 def test_tdp_adapter():
     raw_data_path = "tests/data/tdp_morfometria_albatros.csv"
     obtained = raw_data_adapter(raw_data_path)
+    obtained_columns = obtained.columns
+    assert "bill_length" in obtained_columns
